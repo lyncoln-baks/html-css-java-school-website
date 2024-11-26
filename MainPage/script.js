18672bc7e8f315510ef6f81491bbdd635a9c6231
lyncoln-baks <bakslyncoln@gmail.com>
2024-11-26 20:18:16 -0300

    Add files via upload

---
+++ MainPage/script.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location.href="http://127.0.0.1:5500/home.html"
+}
+
+function download(){
+    window.location.href="http://127.0.0.1:5500/download.html"
+}
+
+function contact(){
+    window.location.href="http://127.0.0.1:5500/contact.html"
+}
+
+function account(){
+    window.location.href="http://127.0.0.1:5500/account.html"
+}
